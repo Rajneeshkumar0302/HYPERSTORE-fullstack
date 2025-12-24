35faa0e8601c23920e06b08efbd4cb62b7eb8dd0
Rajneesh kumar <kranjish0@gmail.com>
2025-12-25 01:44:00 +0530

    Refactor hyperstore.js for cleaner code
    Removed unused imports and commented-out code. Updated CORS origin to allow all origins.

--- ecommerceserver/hyperstore.js
+++ ecommerceserver/hyperstore.js
@@ -1,30 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express'
-import fs from 'fs';    
+import express from 'express'    
 import mongoose from "mongoose";
 import cors from "cors"
 import Product from "./models/Productmodel.js";
-import dotenv from "dotenv";
 import authRoutes from "./Routes/authRoutes.js";
 import adminRoutes from "./Routes/adminRoutes.js";
 import productRoutes from "./Routes/ProductRoutes.js";
  
 // ===== CONFIG =====
-
-dotenv.config();
 const app = express();
 const PORT =process.env.PORT || 3000;
 
 // ===== MIDDLEWARE =====
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,  }));
    
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 // ===== MongoDB Connection =====
 
@@ -41,27 +38,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/admin",adminRoutes);
 app.use("/api/products",productRoutes);
 
-// app.post('/api/cart', async(req,res)=>{
+// app.post('/api/categories', async (req,res)=>{
 //   try {
-//     const savedItem = await Product.create({...req.body,category: "Cart"}); // Save to MongoDB          CART
+//     const savedItem = await Categories.create(req.body); // Save to MongoDB           
 //     res.json(savedItem);
 //   } catch(err) {
 //     res.status(500).json({ error: 'Server error' });
 //   }
 // });
-
-
-app.post('/api/categories', async (req,res)=>{
-  try {
-    const savedItem = await Categories.create(req.body); // Save to MongoDB            CATEGORIES
-    res.json(savedItem);
-  } catch(err) {
-    res.status(500).json({ error: 'Server error' });
-  }
-});
-
-
-
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT} `)     
