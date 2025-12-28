10deba5dce1f35f139f98c25568f7bfafb238224
Rajneesh kumar <kranjish0@gmail.com>
2025-12-28 14:29:51 +0530

    Change header text from 'Women' to 'Furniture'

--- HyperstoreFRONTEND/src/HEADER/Furniture.jsx
+++ HyperstoreFRONTEND/src/HEADER/Furniture.jsx
@@ -33,7 +33,7 @@ const Furniture = () => {
          className="text-[clamp(0.85rem,4vw,9rem)] font-bold text-center"
          style={{ marginBottom: "3%" }}
        >
-         Women
+         Furniture
        </h1>
  
        <div
